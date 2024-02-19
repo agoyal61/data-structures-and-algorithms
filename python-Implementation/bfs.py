783531209545429f4fe477fb94811c8f7027036f
aditya <agoyal61@asu.edu>
2024-02-19 03:40:31 -0700

    add breadth first search implementation

---
+++ python-Implementation/bfs.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+def bfs(g,start):
+    vis=set()
+    q=deque([start])
+    vis.add(start)
+    while q:
+        node=q.popleft()
+        print(node,end=' ')
+        for nbr in g[node]:
+            if nbr not in vis:
+                q.append(nbr)
+                vis.add(nbr)
+    return vis
+
+g={
+    0:[1,2],
+    1:[0,3,4],
+    2:[0,4],
+    3:[1],
+    4:[1,2]
+}
+bfs(g,0)
