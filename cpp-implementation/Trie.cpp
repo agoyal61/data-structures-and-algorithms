bb0fd3e0bdaa5a0c3c9705952ee40dca895b2865
aditya <agoyal61@asu.edu>
2024-02-21 15:12:55 -0700

    update trie

--- cpp-implementation/Trie.cpp
+++ cpp-implementation/Trie.cpp
@@ -5,3 +5,41 @@ struct TrieNode {
     int count=0;
     unordered_map<int,TrieNode*> children;
 };
+
+// Node definition of a trie
+struct Node {
+    bool isWord = false;
+    vector<Node *> children{vector<Node *>(26, NULL)};
+} * Root, *curr;
+
+class TrieNode {
+    public:
+        bool isEndOfWord;
+        unordered_map<char, TrieNode*> children;
+        TrieNode() : isEndOfWord(false) {};
+};
+
+class WordDictionary {
+private:
+    TrieNode *root;
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    void addWord(string word) {
+        TrieNode *node=root;
+    }
+    bool search(string word) {
+    }
+}
+
+struct TrieNode {
+    TrieNode* children[26];
+    string word;
+    TrieNode(){
+        for(auto& child:children){
+            child=nullptr;
+            word="";
+        } 
+    }
+};
