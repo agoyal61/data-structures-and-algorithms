1ebdb0c084b2bde2b86160263d9d3a0a6d17fa41
aditya <agoyal61@asu.edu>
2024-03-16 18:42:43 -0700

    add trie implementation

--- cpp-implementation/Trie.cpp
+++ cpp-implementation/Trie.cpp
@@ -43,3 +43,57 @@ struct TrieNode {
         } 
     }
 };
+
+// Trie implementation
+
+class TrieNode {
+    public:
+        bool isEndOfWord;
+        unordered_map<char, TrieNode*> children;
+        TrieNode() : isEndOfWord(false) {};
+};
+
+class WordDictionary {
+private:
+    TrieNode *root;
+public:
+    WordDictionary() {
+        root = new TrieNode();
+    }
+    bool searchInNode(string& word, int index, TrieNode *node){
+        if(index == word.size()){
+            return node->isEndOfWord;
+        }
+        char currentchar = word[index];
+        if(currentchar != '.'){
+            if(node->children.find(currentchar)==node->children.end()){
+                return false;
+            }else{
+               return searchInNode(word,index+1,node->children[currentchar]); 
+            }
+        } else {
+            for(auto &pair: node->children){
+                if(searchInNode(word, index+1, pair.second)){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
+    void addWord(string word) {
+        TrieNode *node=root;
+        for(char ch:word){
+            if(node->children.find(ch)==node->children.end()){
+                node->children[ch]= new TrieNode();
+            }
+            node=node->children[ch];
+        }
+        node->isEndOfWord=true;
+    }
+    
+    bool search(string word) {
+        return searchInNode(word, 0, root);
+    }
+};
+
