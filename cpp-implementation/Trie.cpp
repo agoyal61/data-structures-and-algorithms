1e83ab4334bcf9956b08ae43c3c8de39a48fec92
aditya <agoyal61@asu.edu>
2024-03-16 18:57:58 -0700

    add trie implementation

--- cpp-implementation/Trie.cpp
+++ cpp-implementation/Trie.cpp
@@ -97,3 +97,53 @@ public:
     }
 };
 
+
+// implementation of trie
+class Trie {
+private:
+    struct TrieNode {
+        bool isEndOfWord;
+        unordered_map<char, TrieNode*> children;
+        TrieNode() : isEndOfWord(false) {}
+    };
+    
+    TrieNode* root;
+    
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+    
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            if (node->children.find(ch) == node->children.end()) {
+                node->children[ch] = new TrieNode();
+            }
+            node = node->children[ch];
+        }
+        node->isEndOfWord = true;
+    }
+    
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            if (node->children.find(ch) == node->children.end()) {
+                return false;
+            }
+            node = node->children[ch];
+        }
+        return node->isEndOfWord;
+    }
+    
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char ch : prefix) {
+            if (node->children.find(ch) == node->children.end()) {
+                return false;
+            }
+            node = node->children[ch];
+        }
+        return true;
+    }
+};
