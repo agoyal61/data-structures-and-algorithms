e1f9775117b27286bc5b7c25d879675665029995
aditya <agoyal61@asu.edu>
2024-03-19 14:49:27 -0700

    update segment tree

--- cpp-implementation/SegmentTree.cpp
+++ cpp-implementation/SegmentTree.cpp
@@ -63,3 +63,91 @@ public:
         return sumRangeSegmentTree(0,0,n-1,left,right);
     }
 };
+
+/*
+Segment Tree is a data structure that allows answering range queries over an array effectively, while still being flexible enough to allow modifying the array. This includes finding the
+ sum of consecutive array elements a range query, and modifying the value of a specific element in the array.
+
+Segment Tree is used in cases where there are multiple range queries on array and modifications of elements of the same array. For example, finding the sum of all the elements in an array from indices 
+ to , or finding the minimum (famously known as Range Minumum Query problem) of all the elements in an array from indices  to . These problems can be easily solved with one of the most versatile data structures, Segment Tree.
+
+Height of segment tree is  log(n) base 2 where n is the number of elements in the array. Since the segment tree is a full binary tree, the number of nodes at each level is double the number of nodes at the previous level. So,
+
+In each step, the segment is divided into half and the two children represent those two halves. So the height of the segment tree will be log(n) base 2. There are n leaves in the segment tree, 
+each representing an element of the array. The number of internal nodes is n-1. So, the total number of nodes are 2n-1.
+
+*/
+
+// Since Segment Tree is a binary tree. 2 * node will represent the left child and 2 * node + 1 will represent the right child. start and end represent the interval represented by the current node.
+// Complexity of build is O(n).
+void build(int node, int start, int end)
+{
+    if(start == end)
+    {
+        // Leaf node will have a single element
+        tree[node] = A[start];
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        // Recurse on the left child
+        build(2*node, start, mid);
+        // Recurse on the right child
+        build(2*node+1, mid+1, end);
+        // Internal node will have the sum of both of its children
+        tree[node] = tree[2*node] + tree[2*node+1];
+    }
+}
+
+// Complexity of update will be O(log n).
+void update(int node, int start, int end, int idx, int val)
+{
+    if(start == end)
+    {
+        // Leaf node
+        A[idx] += val;
+        tree[node] += val;
+    }
+    else
+    {
+        int mid = (start + end) / 2;
+        if(start <= idx and idx <= mid)
+        {
+            // If idx is in the left child, recurse on the left child
+            update(2*node, start, mid, idx, val);
+        }
+        else
+        {
+            // if idx is in the right child, recurse on the right child
+            update(2*node+1, mid+1, end, idx, val);
+        }
+        // Internal node will have the sum of both of its children
+        tree[node] = tree[2*node] + tree[2*node+1];
+    }
+}
+/*
+To query on a given range, check 3 conditions.
+1. Range represented by a node is completely inside the given range
+2. Range represented by a node is completely outside the given range
+3. Range represented by a node is partially inside and partially outside the given range
+*/
+
+// complexity of query will be O(log n).
+int query(int node, int start, int end, int l, int r)
+{
+    if(r < start or end < l)
+    {
+        // range represented by a node is completely outside the given range
+        return 0;
+    }
+    if(l <= start and end <= r)
+    {
+        // range represented by a node is completely inside the given range
+        return tree[node];
+    }
+    // range represented by a node is partially inside and partially outside the given range
+    int mid = (start + end) / 2;
+    int p1 = query(2*node, start, mid, l, r);
+    int p2 = query(2*node+1, mid+1, end, l, r);
+    return (p1 + p2);
+}
--- notes/problem-concepts-notes/probelm-concept-notes.md
+++ notes/problem-concepts-notes/probelm-concept-notes.md
@@ -1,2 +1,11 @@
 
 - transpose of a matrix : flip rows and columns of matrix
+
+- For each square on a given diagonal, the difference between the row and column indices (row - col) will be constant.
+
+- For each square on a given anti-diagonal, the sum of the row and column indexes (row + col) will be constant. If you were to start at the highest square in an anti-diagonal and move downwards, the row index increments by 1 (row + 1), and the column index decrements by 1 (col - 1). These cancel each other out.
+
+-  total lowerDiagonal (n-1+row-col) = total upper diagonal (row+col) = 2*n-1 , where n is dimension of grid or board
+- Box index sudoku: box_index = (row / 3) * 3 + column / 3
+backtrack(row = 0, col = 0) , If we're on the last cell row == 8, col == 8: That means that we've solved the sudoku.
+
